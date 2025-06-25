93cb6b78227245b808e523f2724dfb5f425dd883
Daniel <grabardm@ml.dev.ru>
2025-06-26 02:36:46 +1000

    Add generate calibration board

---
+++ calibration/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from .board import (CharucoBoard,
+                    ArucoBoard,
+                   Checkerboard, 
+                   CircleGridBoard, 
+                   PDFExporter)
+
+__all__ = [
+    "CharucoBoard",
+    "ArucoBoard",
+    "Checkerboard",
+    "CircleGridBoard",
+    "PDFExporter"
+]
